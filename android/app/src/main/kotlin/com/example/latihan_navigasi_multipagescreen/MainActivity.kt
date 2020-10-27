43a0758f6772da2be5c55d22a22646e247b68c23
luizmirrelll <aloysius.mirel96@gmail.com>
2020-10-27 22:59:06 +0700

    Navigasi-Multi-Page-Screen

---
+++ android/app/src/main/kotlin/com/example/latihan_navigasi_multipagescreen/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.latihan_navigasi_multipagescreen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/second_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Main Page"),
+      ),
+      body: Center(
+        child: RaisedButton(
+            child: Text("back"),
+            onPressed: () {
+              Navigator.pop(context);
+            }),
+      ),
+    );
+  }
+}
